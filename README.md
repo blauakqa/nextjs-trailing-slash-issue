238d807b495292e962b429ab8e4ea46696ac8bff
Ben <ben.lau@akqa.com>
2023-09-01 11:44:49 +1000

    trailing slash removal issue

---
+++ README.md
@@ -0,0 +1,1 @@
+NextJS trailing slash removed issue
---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  skipTrailingSlashRedirect: true,
+  /* config options here */
+}
+ 
+module.exports = nextConfig
